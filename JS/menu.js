8b61ede2fa18ebbce3c2b79a03faa6776b9a1bc1
Guilherme Silva <guilhermebaos@gmail.com>
2020-08-05 15:32:31 +0100

    Mudança do Menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,31 +1,38 @@
+// Se necessário, fazer de forma a não ter de fazer mais Requests ao Server
+
 let pronto = false
-let aberto = ''
+let aberto = 'teoria.html'
+
+var alvo, liTeoria, liMaterial, liProcedimento, liResultados
 
 function carregar(ficheiro) {
     if (pronto == false) {
-        var alvo = document.getElementById('alvo')
+        alvo = document.getElementById('alvo')
 
-        var liTeoria = document.getElementById('teoria')
-        var liMaterial = document.getElementById('material')
-        var liProcedimento = document.getElementById('procedimento')
+        liTeoria = document.getElementById('teoria')
+        liMaterial = document.getElementById('material')
+        liProcedimento = document.getElementById('procedimento')
+        liResultados = document.getElementById('resultados')
+
+        pronto = true
     }
     if (aberto != ficheiro) {
         alvo.setAttribute('incluir', ficheiro)
         incluirHTML()
         aberto = ficheiro
-    } else {
-        alvo.innerHTML = ''
-        aberto = ''
-    }
 
-    liTeoria.style.textDecoration = ''
-    liMaterial.style.textDecoration = ''
-    liProcedimento.style.textDecoration = ''
-    if (aberto == 'teoria.html') {
-        liTeoria.style.textDecoration = 'underline'
-    } else if (aberto == 'material.html') {
-        liMaterial.style.textDecoration = 'underline'
-    } else if (aberto == 'procedimento.html') {
-        liProcedimento.style.textDecoration = 'underline'
+        liTeoria.style.textDecoration = ''
+        liMaterial.style.textDecoration = ''
+        liProcedimento.style.textDecoration = ''
+        liResultados.style.textDecoration = ''
+        if (aberto == 'teoria.html') {
+            liTeoria.style.textDecoration = 'underline'
+        } else if (aberto == 'material.html') {
+            liMaterial.style.textDecoration = 'underline'
+        } else if (aberto == 'procedimento.html') {
+            liProcedimento.style.textDecoration = 'underline'
+        } else if (aberto == 'resultados.html') {
+            liResultados.style.textDecoration = 'underline'
+        }
     }
 }
