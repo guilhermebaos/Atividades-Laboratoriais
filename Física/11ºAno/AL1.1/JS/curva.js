f442d6dba1253cfe6d142700166f7ca8aab82261
Guilherme Silva <guilhermebaos@gmail.com>
2020-08-18 22:38:53 +0100

    Ficheiros AL1.1 F11

---
+++ Física/11ºAno/AL1.1/JS/curva.js
@@ -0,0 +1,10 @@
+// Definir Constantes
+const g = 9.80665 // Aceleração Gravitaconal
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let F11_AL11 = {
+    preparado: false,
+    divCurva: ''
+}
