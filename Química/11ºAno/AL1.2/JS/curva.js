3464ac9cdf2324d04fceb82a2c68b1fb5719de10
Guilherme Silva <guilhermebaos@gmail.com>
2020-09-02 09:44:03 +0100

    Ficheiros Vazios AL1.2 Q11

---
+++ Química/11ºAno/AL1.2/JS/curva.js
@@ -0,0 +1,26 @@
+// Definir constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let Q11_AL12 = {
+    preparado: false,
+}
+
+
+
+function prepararResultados() {
+    if (Q11_AL12.preparado) {
+        return
+    }
+    Q11_AL12.preparado = true
+    
+    // Selecionar Sliders
+    
+    // Selecionar os Spans com os Valores dos Sliders
+
+    // Selecionar os Spans da Tabela
+    
+    // Atualizar os Sliders
+}
