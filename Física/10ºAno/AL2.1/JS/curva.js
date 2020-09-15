e994df0acbd0166037b6f40bf6019efd1974a2e3
Guilherme Silva <guilhermebaos@gmail.com>
2020-09-15 22:19:40 +0100

    File Structure AL2.1 F10

---
+++ Física/10ºAno/AL2.1/JS/curva.js
@@ -0,0 +1,26 @@
+// Definir Constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let F10_AL21 = {
+    preparado: false,
+    divCurva: ''
+}
+
+
+function prepararResultados() {
+    if (F10_AL21.preparado) {
+        return
+    }
+    F10_AL21.preparado = true
+    
+    // Selecionar Sliders
+    
+    // Selecionar os Spans com os Valores dos Sliders
+    
+    // Selecionar a div onde vai parar a curva
+
+    // Atualizar os Sliders
+}
