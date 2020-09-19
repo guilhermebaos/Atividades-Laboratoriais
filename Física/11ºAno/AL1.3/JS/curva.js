95aeb2dec9e5eb0e056bb2727261a9fccca10d30
Guilherme Silva <guilhermebaos@gmail.com>
2020-09-19 15:44:16 +0100

    Ficheiros vazios AL1.3

---
+++ Física/11ºAno/AL1.3/JS/curva.js
@@ -0,0 +1,31 @@
+// Definir Constantes
+const g = 9.80665   // Aceleração Gravitaconal
+const CAM = 0.3     // Razão máxima entre a Resultante das Forças de Atrito e o Peso do Corpo Suspenso
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let F11_AL13 = {
+    preparado: false,
+    divCurva: ''
+}
+
+
+
+function prepararResultados() {
+    if (F11_AL13.preparado) {
+        return
+    }
+    F11_AL13.preparado = true
+
+    // Selecionar Sliders
+
+    // Selecionar os Spans com os Valores dos Sliders
+
+    // Selecionar a div que vai ter a Curva
+
+    // Selecionar os Spans com os Resultados da Tabela
+
+    // Atualizar os Sliders
+}
