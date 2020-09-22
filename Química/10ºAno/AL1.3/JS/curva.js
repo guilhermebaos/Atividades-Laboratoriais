39bbfaef657bf293c0cbbaa3cf403fed9dcfac66
Guilherme Silva <guilhermebaos@gmail.com>
2020-09-22 19:56:33 +0100

    Teoria AL1.3 Q10

--- Química/10ºAno/AL1.3/JS/curva.js
+++ Química/10ºAno/AL1.3/JS/curva.js
@@ -5,6 +5,10 @@ let Q10_AL13 = {
     preparado: false,
 }
 
+let metalArray
+let metalEscolhidoPos = 0
+
+
 
 function prepararResultados() {
     if (Q10_AL13.preparado) {
@@ -16,13 +20,46 @@ function prepararResultados() {
     
     // Selecionar os Spans com os Valores dos Sliders
 
+    // Selecionar os Butões
+    metalArray = document.getElementsByName('metalPicnometria')
+
     // Selecionar os Spans com os Resultados da Tabela
     
     // Atualizar os Sliders
 }
 
-// Mostra os Resultados na Tabela, Nome do Sal e Cor da Chama
-function curva() {
+
+// Altera o Metal escolhido, bem como a aparência dos butões
+function escolherMetal(pos) {
+
+    metalArray[metalEscolhidoPos].className = 'escolha'
+    metalArray[pos].className = 'escolha-atual'
+
+    metalEscolhidoPos = pos
+
+    curva()
 }
 
-// Adicionar Imagens da Chama, quando as tiver
+
+// Mostra os Resultados na Tabela
+function curva() {
+    if (metalEscolhidoPos == '0') {
+        salEscolhidoResp.innerText = 'Cloreto de Sódio'
+        corChamaResp.innerText = 'Amarelo'
+    } else if (metalEscolhidoPos == '1') {
+        salEscolhidoResp.innerText = 'Cloreto de Cálcio'
+        corChamaResp.innerText = 'Vermelho Claro'
+    } else if (metalEscolhidoPos == '2') {
+        salEscolhidoResp.innerText = 'Cloreto de Potássio'
+        corChamaResp.innerText = 'Violeta'
+    } else if (metalEscolhidoPos == '3') {
+        salEscolhidoResp.innerText = 'Cloreto de Bário'
+        corChamaResp.innerText = 'Verde'
+    } else if (metalEscolhidoPos == '4') {
+        salEscolhidoResp.innerText = 'Cloreto de Lítio'
+        corChamaResp.innerText = 'Vermelho Escuro'
+    } else if (metalEscolhidoPos == '5') {
+        salEscolhidoResp.innerText = 'Cloreto de Cobre (II)'
+        corChamaResp.innerText = 'Verde Claro'
+    }
+}
