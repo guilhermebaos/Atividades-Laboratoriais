067b534cd21195c2c34811f3d2fd6b2df07c1659
Guilherme Silva <guilhermebaos@gmail.com>
2020-09-22 16:21:17 +0100

    Ficheiros Vazios AL1.3 Q10

---
+++ Química/10ºAno/AL1.3/JS/curva.js
@@ -0,0 +1,28 @@
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let Q10_AL13 = {
+    preparado: false,
+}
+
+
+function prepararResultados() {
+    if (Q10_AL13.preparado) {
+        return
+    }
+    Q10_AL13.preparado = true
+
+    // Selecionar Sliders
+    
+    // Selecionar os Spans com os Valores dos Sliders
+
+    // Selecionar os Spans com os Resultados da Tabela
+    
+    // Atualizar os Sliders
+}
+
+// Mostra os Resultados na Tabela, Nome do Sal e Cor da Chama
+function curva() {
+}
+
+// Adicionar Imagens da Chama, quando as tiver
