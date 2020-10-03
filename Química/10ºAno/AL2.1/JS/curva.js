9add7906bd7c049c7034dd5408a9cd2f43b63004
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-03 15:16:33 +0100

    Ficheiros AL2.1 Q10 vazios

---
+++ Química/10ºAno/AL2.1/JS/curva.js
@@ -0,0 +1,29 @@
+// Definir Constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let Q10_AL21 = {
+    preparado: false,
+}
+
+
+function prepararResultados() {
+    if (Q10_AL21.preparado) {
+        return
+    }
+    Q10_AL21.preparado = true
+
+
+    // Selecionar Sliders
+    
+    // Selecionar os Spans com os Valores dos Sliders
+
+    // Selecionar os Spans com os Resultados da Tabela
+
+    
+    // Atualizar os Sliders
+
+    curva()
+}
