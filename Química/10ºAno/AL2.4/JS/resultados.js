afaf2817f27fca7e699ec44a6e0e0dfb091249a2
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-13 08:50:48 +0100

    Ficheiros vazios AL2.4 Q10

---
+++ Química/10ºAno/AL2.4/JS/resultados.js
@@ -0,0 +1,24 @@
+// Definir Constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let Q10_AL24 = {
+    preparado: false,
+}
+
+
+function prepararResultados() {
+    if (Q10_AL24.preparado) {
+        return
+    }
+    Q10_AL24.preparado = true
+
+    curva()
+}
+
+
+// Mostrar o Resultado
+function curva() {
+}
