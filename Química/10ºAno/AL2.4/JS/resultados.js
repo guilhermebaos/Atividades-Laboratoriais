fcdb95081f4a685b77715ca86771a44338f5a76e
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-14 23:07:19 +0100

    Butões Resultados AL2.4 Q10

--- Química/10ºAno/AL2.4/JS/resultados.js
+++ Química/10ºAno/AL2.4/JS/resultados.js
@@ -8,6 +8,16 @@ let Q10_AL24 = {
     preparado: false,
 }
 
+let tubos
+
+let embrulhos = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+}
+
 
 function prepararResultados() {
     if (Q10_AL24.preparado) {
@@ -15,7 +25,38 @@ function prepararResultados() {
     }
     Q10_AL24.preparado = true
 
+    // Selecionar os Butões
+    tubos = document.getElementsByName('tubo')
+
     curva()
+}
+
+
+// Alterar os embrulhos dos Tubos e as aparências dos Butões
+function escolherTubo(letraTubo, embrulhoEscolhido) {
+    let posLetra, posEmbrulho
+
+    // Retirar o embrulho ao tubo que o tinha antes
+    let nomesTubos = ['A', 'B', 'C', 'D', 'E']
+    for (let key in nomesTubos) {
+        if (embrulhos[nomesTubos[key]] == embrulhoEscolhido) {
+            tubos[embrulhoEscolhido * 5 + nomesTubos[key].codePointAt(0) - 65].className = 'escolha'
+            tubos[embrulhos[letraTubo] * 5 + nomesTubos[key].codePointAt(0) - 65].className = 'escolha-atual'
+            embrulhos[nomesTubos[key]] = embrulhos[letraTubo]
+            break
+        }
+    }
+    
+    // Obter as posições da letra (coluna) e embrulho (linha)
+    posLetra = letraTubo.codePointAt(0) - 65
+    posEmbrulho = embrulhos[letraTubo]
+
+    // Alterar os aspetos do butão da letra selecionada
+    tubos[posEmbrulho * 5 + posLetra].className = 'escolha'
+    tubos[embrulhoEscolhido * 5 + posLetra].className = 'escolha-atual'
+
+    // Atualizar o Objeto que guarda os embrulhos de cada tubo
+    embrulhos[letraTubo] = embrulhoEscolhido
 }
 
 
