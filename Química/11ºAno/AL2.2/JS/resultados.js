6c6bcd2dee57051366fcf884f733f2e809411a04
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-17 10:54:19 +0100

    Ficheiros Vazios AL2.2 Q11

---
+++ Química/11ºAno/AL2.2/JS/resultados.js
@@ -0,0 +1,24 @@
+// Definir Constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let Q11_AL22 = {
+    preparado: false,
+}
+
+
+function prepararResultados() {
+    if (Q11_AL22.preparado) {
+        return
+    }
+    Q11_AL22.preparado = true
+
+    curva()
+}
+
+
+// Mostrar o Resultado
+function curva() {
+}
