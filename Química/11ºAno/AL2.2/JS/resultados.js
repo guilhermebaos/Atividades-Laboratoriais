9120a1246d5dca2fa6e2d4bd2bb7318c12d6ccdd
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-18 16:43:09 +0100

    Resultados AL2.2 Q11
    + Correções Teoria e Resultados AL2.1 Q10

--- Química/11ºAno/AL2.2/JS/resultados.js
+++ Química/11ºAno/AL2.2/JS/resultados.js
@@ -8,6 +8,14 @@ let Q11_AL22 = {
     preparado: false,
 }
 
+let metais
+let cats
+
+let parEscolhido = [0, 1]
+
+let resultadoAntigo = '01'
+let resultadoNovo
+
 
 function prepararResultados() {
     if (Q11_AL22.preparado) {
@@ -15,10 +23,36 @@ function prepararResultados() {
     }
     Q11_AL22.preparado = true
 
-    curva()
+    // Selecionar os Butões
+    metais = document.getElementsByName('metal')
+    cats = document.getElementsByName('catião')
+}
+
+
+// Escolher o Metal
+function escolherMet(num) {
+    metais[parEscolhido[0]].className = 'escolha'
+    metais[num].className = 'escolha-atual'
+    parEscolhido[0] = num
+}
+
+
+// Escolher o Catião em Solução Aquosa
+function escolherCat(num) {
+    cats[parEscolhido[1]].className = 'escolha'
+    cats[num].className = 'escolha-atual'
+    parEscolhido[1] = num
 }
 
 
 // Mostrar o Resultado
 function curva() {
+    if (parEscolhido[0] == parEscolhido[1]) {
+        resultadoNovo = 'erro'
+    } else resultadoNovo = parEscolhido.toString().replaceAll(',', '')
+    if (resultadoAntigo != resultadoNovo) {
+        mostrarExtra(resultadoAntigo)
+        mostrarExtra(resultadoNovo)
+        resultadoAntigo = resultadoNovo
+    }
 }
