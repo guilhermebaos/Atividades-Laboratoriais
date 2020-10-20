50ff4f2946d7a57734d31b4f84851c1a82441250
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-20 14:37:03 +0100

    Ficheiros Vazios AL2.3 Q11
    + Correções AL2.1 Q11 (visto ser a antiga AL2.2 Q11)

---
+++ Química/11ºAno/AL2.3/JS/resultados.js
@@ -0,0 +1,40 @@
+// Definir constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let Q11_AL23 = {
+    preparado: false,
+    divCurva: ''
+}
+
+
+function prepararResultados() {
+    if (Q11_AL23.preparado) {
+        return
+    }
+    Q11_AL23.preparado = true
+    
+    // Selecionar os butões
+
+    // Selecionar Sliders
+
+    // Selecionar os Spans com os Valores dos Sliders
+
+    // Selecionar a div onde vai parar a curva
+
+    curva()
+}
+
+
+// Recebe os valores do pH e traça a Curva de pH
+function curva() {
+    // Remover o Canvas antigo
+    Q11_AL23.divCurva.innerHTML = ''
+
+    // Criar o canvas on de vai estar a curva
+    canvasCurva = document.createElement('canvas')
+    canvasCurva.setAttribute('id', 'canvasCurva')
+    Q11_AL23.divCurva.appendChild(canvasCurva)
+}
