b7de6d3df73e620f3204898276a46445c6bb8da9
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-24 17:39:00 +0100

    Ficheiros Vazios AL3.2 F10

---
+++ Física/10ºAno/AL3.2/JS/resultados.js
@@ -0,0 +1,40 @@
+// Definir Constantes
+
+
+// Inicializar Variáveis Globais
+
+// Usar um Objeto para proteger as variáveis com nomes comuns
+let F10_AL32 = {
+    preparado: false,
+    divCurva: ''
+}
+
+
+function prepararResultados() {
+    if (F10_AL32.preparado) {
+        return
+    }
+    
+    // Selecionar Sliders
+
+    // Selecionar os Spans com os Valores dos Sliders
+    
+    // Selecionar a div onde vai parar a curva
+
+    // Atualizar os Sliders
+
+    F10_AL32.preparado = true
+    curva()
+}
+
+
+// Calcular os Pontos do Gráfico 
+function pontos() {
+    // Declarar variáveis e valores iniciais
+}
+
+
+
+// Mostra o Gráfico relacionado com o bloco calorimétrico
+function curva() {
+}
