0c2eb487803bc104f280ba2d694a89c62c46adb3
Guilherme Silva <guilhermebaos@gmail.com>
2020-10-31 22:33:14 +0000

    Correção da função que mostra os extras

--- Física/10ºAno/AL3.3/JS/resultados.js
+++ Física/10ºAno/AL3.3/JS/resultados.js
@@ -12,6 +12,21 @@ let F10_AL33 = {
     divCurva: ''
 }
 
+let massaQuente
+let tempQuente
+let massaFria
+let tempFria
+let massaGelo
+
+let massaQuenteResp
+let tempQuenteResp
+let massaFriaResp
+let tempFriaResp
+let massaGeloResp
+
+let escolhasMistura
+let misturaEscolhida = 0
+
 
 function prepararResultados() {
     if (F10_AL33.preparado) {
@@ -19,19 +34,70 @@ function prepararResultados() {
     }
     
     // Selecionar Sliders
+    massaQuente = document.getElementById('massaQuente')
+    tempQuente = document.getElementById('tempQuente')
+    massaFria = document.getElementById('massaFria')
+    tempFria = document.getElementById('tempFria')
+    massaGelo = document.getElementById('massaGelo')
 
     // Selecionar os Spans com os Valores dos Sliders
+    massaQuenteResp = document.getElementById('massaQuenteValue')
+    tempQuenteResp = document.getElementById('tempQuenteValue')
+    massaFriaResp = document.getElementById('massaFriaValue')
+    tempFriaResp = document.getElementById('tempFriaValue')
+    massaGeloResp = document.getElementById('massaGeloValue')
 
     // Selecionar os Butões
+    escolhasMistura = document.getElementsByName('escolhaMistura')
 
     // Selecionar os spans com os Resultados da Tabela
 
     // Selecionar a div onde vai parar a curva
 
     // Atualizar os Sliders
+    massaQuente.oninput = function atualizarMassaQuente() {
+        let massaQuenteValue = massaQuente.value / 10
+    
+        massaQuenteResp.innerText = `${massaQuenteValue.toFixed(1)}`
+    }
+    tempQuente.oninput = function atualizarTempQuente() {
+        let tempQuenteValue = tempQuente.value / 10
+    
+        tempQuenteResp.innerText = `${tempQuenteValue.toFixed(1)}`
+    }
+    massaFria.oninput = function atualizarMassaFria() {
+        let massaFriaValue = massaFria.value / 10
+    
+        massaFriaResp.innerText = `${massaFriaValue.toFixed(1)}`
+    }
+    tempFria.oninput = function atualizarTempFria() {
+        let tempFriaValue = tempFria.value / 10
+    
+        tempFriaResp.innerText = `${tempFriaValue.toFixed(1)}`
+    }
+    massaGelo.oninput = function atualizarMassaGelo() {
+        let massaGeloValue = massaGelo.value / 10
+    
+        massaGeloResp.innerText = `${massaGeloValue.toFixed(1)}`
+    }
 
     F10_AL33.preparado = true
     curva()
+}
+
+
+// Selecionar o que vai ser misturado e alterar a aparência dos Butões
+function escolherMistura(num) {
+    if (num != misturaEscolhida) {
+        // Desselecionar escolha anterior
+        mostrarExtra(`mistura${misturaEscolhida}`)
+        escolhasMistura[misturaEscolhida].className = 'escolha'
+
+        // Selecinar Nova Escolha
+        misturaEscolhida = num
+        mostrarExtra(`mistura${misturaEscolhida}`)
+        escolhasMistura[misturaEscolhida].className = 'escolha-atual'
+    }
 }
 
 
